bdde2049cd898931dcaa5895280434fd85115933
Humphrey <humphrey.muhuri@student.moringaschool.com>
2023-05-13 07:31:47 +0300

    Completed project

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MovieList";
+import MovieShow from "./MovieShow";
 
-function MoviesPage({ movies }) {
+const MoviesPage = ({ movies }) => {
+  const { url } = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={url}>
+        <h2>Movie List</h2>
+      </Route>
+
+      <Route path={`${url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
-}
+};
+
 export default MoviesPage;
